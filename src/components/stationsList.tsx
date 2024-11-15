442c0f80e7d25c0777c6a54d9f5e8ca970e63570
김지훈 <kim@jihun.io>
2024-11-15 18:58:03 +0900

    방송국 로고를 서버로부터 동기화

---
+++ src/components/stationsList.tsx
@@ -0,0 +1,167 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { Embla, EmblaSlide, EmblaContainer } from "../components/embla";
+import { useAudioStore } from "../store/useAudioStore";
+import { useRegionStore } from "@/store/useRegionStore";
+
+interface Station {
+  category: string;
+  id: string;
+  name: string;
+  type: string;
+  stations: {
+    city: string;
+    name: string;
+  }[];
+  channels?: {
+    id: string;
+    name: string;
+  }[];
+}
+
+interface StationCategory {
+  result: Station[];
+}
+
+export default function StationsList() {
+  const { recentStations, setStation } = useAudioStore();
+  const { selectedRegions, setSelectedRegions } = useRegionStore();
+
+  const [stations, setStations] = useState<Station[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    let isMounted = true;
+
+    const fetchStations = async () => {
+      try {
+        // selectedRegions 값을 직접 사용
+        const selectedRegionsStr = selectedRegions.join(",");
+
+        const res = await fetch(
+          `/radio-proxy/stations?region=korea,${selectedRegionsStr}`
+        );
+        if (!res.ok) throw new Error("Failed to fetch");
+        const data = await res.json();
+
+        if (isMounted) {
+          setStations(data || []);
+          setError(null);
+        }
+      } catch (error) {
+        if (isMounted) {
+          setError(
+            error instanceof Error
+              ? error.message
+              : "데이터를 불러오는데 실패했습니다"
+          );
+          console.error(error);
+        }
+      } finally {
+        if (isMounted) {
+          setIsLoading(false);
+        }
+      }
+    };
+
+    fetchStations();
+
+    return () => {
+      isMounted = false;
+    };
+  }, [selectedRegions]); // selectedRegions를 의존성 배열에 추가
+
+  if (error) return <div>Error: {error}</div>;
+  if (isLoading)
+    return (
+      <section className="">
+        <h2 className="font-bold text-xl">로드 중...</h2>
+      </section>
+    );
+  if (!stations.length) return <div>방송국 정보가 없습니다.</div>;
+
+  const groupedStations = stations.reduce((acc, station) => {
+    // category가 없는 경우 방송사 이름을 카테고리로 사용
+    let category = station.category || station.name;
+
+    if (category === "religion") {
+      category = "종교 방송";
+    } else if (category === "local") {
+      category = "지역 방송";
+    } else if (category === "extra") {
+      category = "기타";
+    }
+
+    if (!acc[category]) {
+      acc[category] = [];
+    }
+    acc[category].push(station);
+    return acc;
+  }, {} as Record<string, Station[]>);
+
+  return Object.entries(groupedStations).map(([category, stationList]) => (
+    <section key={category}>
+      <h2 className="py-4 text-2xl font-bold">{category}</h2>
+      <Embla>
+        <EmblaContainer>
+          {stationList.map((station) => {
+            if (station.channels) {
+              return station.stations.map((localChannel) =>
+                station.channels!.map((channel) => (
+                  <EmblaSlide
+                    key={`${station.id}-${channel.name}`}
+                    onClick={() =>
+                      setStation({
+                        id: `${station.id.toLowerCase()}${channel.id.toLowerCase()}`,
+                        streamUrl: `?stn=${station.id.toLowerCase()}&ch=${
+                          channel.id
+                        }${
+                          localChannel.city === "korea" ||
+                          localChannel.city === "seoul"
+                            ? ""
+                            : `&city=${localChannel.city}`
+                        }`,
+                        name: `${localChannel.name} ${channel.name}`,
+                        logo: `/images/stations/${station.id.toLowerCase()}${channel.id.toLowerCase()}.svg`,
+                      })
+                    }
+                  >
+                    <img
+                      className="rounded-lg mb-2"
+                      src={`/images/stations/${station.id.toLowerCase()}${channel.id.toLowerCase()}.svg`}
+                      alt={station.name}
+                    />
+                    <h3 className="text-sm">{`${localChannel.name} ${channel.name}`}</h3>
+                  </EmblaSlide>
+                ))
+              );
+            } else {
+              return station.stations.map((channel) => (
+                <EmblaSlide
+                  key={station.id}
+                  onClick={() =>
+                    setStation({
+                      id: `${station.id}`,
+                      streamUrl: `?stn=${station.id.toLowerCase()}`,
+                      name: `${channel.name}`,
+                      logo: `/images/stations/${station.id}.svg`,
+                    })
+                  }
+                >
+                  <img
+                    className="rounded-lg mb-2"
+                    src={`/images/stations/${station.id}.svg`}
+                    alt={channel.name}
+                  />
+                  <h3 className="text-sm">{channel.name}</h3>
+                </EmblaSlide>
+              ));
+            }
+          })}
+        </EmblaContainer>
+      </Embla>
+    </section>
+  ));
+}
